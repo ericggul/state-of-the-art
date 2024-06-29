1a9054d2c3713a51d8ed706c6c7a2d591b2d5320
ericggul <57379903+ericggul@users.noreply.github.com>
2024-06-29 21:25:58 +0900

    Fix: Structure Update

--- components/test/fc-3d/1/index.js
+++ components/test/fc-3d/1/index.js
@@ -11,57 +11,7 @@ import { Perf } from "r3f-perf";
 
 import Connections from "./connections";
 
-export const STRUCTURE = [
-  {
-    position: [0, 0, -10],
-    node: {
-      size: [4, 4, 0.3],
-    },
-    unexpandedNode: {
-      size: [8, 8, 0.3],
-    },
-    grid: {
-      xCount: 3,
-      yCount: 2,
-      xInterval: 5,
-      yInterval: 5,
-    },
-    color: "blue",
-  },
-
-  {
-    position: [0, 0, 0],
-    node: {
-      size: [2, 2, 0.3],
-    },
-    unexpandedNode: {
-      size: [8, 8, 0.3],
-    },
-    grid: {
-      xCount: 5,
-      yCount: 5,
-      xInterval: 3,
-      yInterval: 3,
-    },
-    color: "blue",
-  },
-  {
-    position: [0, 0, 10],
-    node: {
-      size: [4, 4, 0.3],
-    },
-    unexpandedNode: {
-      size: [8, 8, 0.3],
-    },
-    grid: {
-      xCount: 3,
-      yCount: 2,
-      xInterval: 5,
-      yInterval: 5,
-    },
-    color: "blue",
-  },
-];
+import { STRUCTURE } from "./structure";
 
 // Main component to render the neural network
 export default function NN3D() {
