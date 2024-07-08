8f3dd43ccecda6f5be74e43148721f6d8fbe7ce7
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-08 18:27:31 +0900

    Fix: SVG Minor Edit

--- components/test/grid/0/index.js
+++ components/test/grid/0/index.js
@@ -6,21 +6,38 @@ import React, { useState, useEffect, useRef } from "react";
 const getRandomDigit = () => Math.floor(Math.random() * 10);
 
 export default function TextGrid() {
+  const [keyPressing, setKeyPressing] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", () => setKeyPressing(true));
+    document.addEventListener("keyup", () => setKeyPressing(false));
+
+    return () => {
+      document.removeEventListener("keydown", () => setKeyPressing(true));
+      document.removeEventListener("keyup", () => setKeyPressing(false));
+    };
+  }, []);
+
   const [numbers, setNumbers] = useState(new Array(100).fill(0).map((_, i) => getRandomDigit()));
 
   useEffect(() => {
+    if (!keyPressing) return;
     const interval = setInterval(() => {
       setNumbers(numbers.map(() => getRandomDigit()));
-    }, 10);
+    }, 20);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [keyPressing]);
 
   return (
     <S.Container>
-      <S.InnerSVG>
+      <S.InnerSVG
+        style={{
+          opacity: keyPressing ? 1 : 0.05,
+        }}
+      >
         {new Array(100).fill(0).map((_, i) => (
           <text key={i} x="5" y={i * 20} color="white">
             {numbers.join("")}
