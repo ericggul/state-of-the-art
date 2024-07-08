550e71d32f3eb9630c51b4946bf1ad2666367b7b
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-08 17:18:48 +0900

    Fix

---
+++ components/test/propagation/0/index.js
@@ -0,0 +1,8 @@
+"use client";
+
+import * as S from "./styles";
+
+// Main component to render the neural network
+export default function Prop() {
+  return <S.Container></S.Container>;
+}
---
+++ components/test/propagation/0/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { FlexCenterStyle, WholeContainer } from "@/styles";
+
+export const Container = styled.div`
+  ${WholeContainer}
+  ${FlexCenterStyle}
+
+  background: #111;
+`;
