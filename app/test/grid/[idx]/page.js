d9e4b468ef65d49f1ae6491c5f5a69a0585da6e2
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-09 16:42:20 +0900

    Fix: Editing Structure

--- app/test/grid/[idx]/page.js
+++ app/test/grid/[idx]/page.js
@@ -6,6 +6,8 @@ import { useParams } from "next/navigation";
 const Grid0 = dynamic(() => import("@/components/test/grid/0"));
 const Grid1 = dynamic(() => import("@/components/test/grid/1"));
 const Grid2 = dynamic(() => import("@/components/test/grid/2"));
+const Grid21 = dynamic(() => import("@/components/test/grid/2-1"));
+const Grid22 = dynamic(() => import("@/components/test/grid/2-2"));
 
 export default function NNPage() {
   const { idx } = useParams();
@@ -15,6 +17,8 @@ export default function NNPage() {
       {idx === "0" && <Grid0 isTesting={true} />}
       {idx === "1" && <Grid1 isTesting={true} />}
       {idx === "2" && <Grid2 isTesting={true} />}
+      {idx === "2-1" && <Grid21 isTesting={true} />}
+      {idx === "2-2" && <Grid22 isTesting={true} />}
     </>
   );
 }
