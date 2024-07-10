286cdb3bdfb14a367f930b4c93917fb4889a7143
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-10 22:02:14 +0900

    Fix: Build Bug Fix

--- app/test/embeddings/page.js
+++ app/test/embeddings/page.js
@@ -7,7 +7,7 @@ const Embeddings = dynamic(() => import("@/foundations/test/embeddings/0"));
 export default function NNPage() {
   return (
     <>
-      <Embeddings />
+      <Embeddings tokens={[]} />
     </>
   );
 }
