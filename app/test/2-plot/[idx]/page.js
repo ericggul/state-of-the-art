3c72c3a75fcd54293663a418de020c2f14d89c4b
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-16 19:36:35 +0900

    Feat: UMAP

--- app/test/2-plot/[idx]/page.js
+++ app/test/2-plot/[idx]/page.js
@@ -3,13 +3,21 @@
 import dynamic from "next/dynamic";
 import { useParams } from "next/navigation";
 
-import { TEST_EMBEDDINGS } from "@/foundations/test/1-relation/constant";
+import { TEST_EMBEDDINGS } from "@/foundations/test/2-plot/constant";
 
-//0: SVG
+//TSNE
 const Tsne0 = dynamic(() => import("@/foundations/test/2-plot/tsne/0"));
+
+//UMAP
+const Umap0 = dynamic(() => import("@/foundations/test/2-plot/umap/0"));
 
 export default function TsnePage() {
   const { idx } = useParams();
 
-  return <>{idx == "tsne-0" && <Tsne0 newEmbeddings={TEST_EMBEDDINGS} />}</>;
+  return (
+    <>
+      {idx == "tsne-0" && <Tsne0 newEmbeddings={TEST_EMBEDDINGS} />}
+      {idx == "umap-0" && <Umap0 newEmbeddings={TEST_EMBEDDINGS} />}
+    </>
+  );
 }
