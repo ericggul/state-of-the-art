f8120519f34e95dd1f9def714d7af903b8fc4414
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-17 15:17:00 +0900

    Feat: 3

--- foundations/test/1-relation/3/index.js
+++ foundations/test/1-relation/3/index.js
@@ -1,5 +1,5 @@
 import * as S from "./styles";
-import { useMemo, useCallback, useState, useEffect } from "react";
+import { useMemo } from "react";
 import usePosCalc from "./usePosCalc";
 import { useComputeMultiCrossSimlarity } from "@/foundations/test/1-relation/utils/useComputeSimilarity";
 
@@ -7,67 +7,71 @@ export default function Layer1({ newMultiEmbeddings }) {
   const embeddingsArr = useMemo(() => newMultiEmbeddings.map((embeddings) => embeddings.embeddings), [newMultiEmbeddings]);
   const tokensArr = useMemo(() => newMultiEmbeddings.map((embeddings) => embeddings.tokens), [newMultiEmbeddings]);
 
-  const crossSimilarityMatrix = useComputeMultiCrossSimlarity({
+  const multiSimilarityMatrix = useComputeMultiCrossSimlarity({
     newMultiEmbeddings,
   });
 
-  console.log(crossSimilarityMatrix);
+  const posCalcArr = usePosCalc({ tokensArr });
+  console.log(posCalcArr);
 
-  // const { wordPosCalc: inputWordPosCalc, wordInterval: inputWordInterval, verticalInterval: inputVerticalInterval } = usePosCalc({ tokens: inputTokens, type: "input" });
-  // const { wordPosCalc: outputWordPosCalc, wordInterval: outputWordInterval, verticalInterval: outputVerticalInterval } = usePosCalc({ tokens: outputTokens, type: "output" });
+  const bezierParams = {
+    controlX1Factor: 0,
+    controlX2Factor: 1,
+    controlY1Factor: 2,
+    controlY2Factor: 8,
+  };
 
-  // // Function to create a smoother cubic Bezier curve path between two points
-  // const createBezierPath = (x1, y1, x2, y2) => {
-  //   const controlX1 = x1 + (x2 - x1) / 2;
-  //   const controlY1 = y1 + inputVerticalInterval;
-  //   const controlX2 = x2 - (x2 - x1) / 2;
-  //   const controlY2 = y2 - outputVerticalInterval;
+  // Function to create a smoother cubic Bezier curve path between two points
+  const createBezierPath = (x1, y1, x2, y2, inputVerticalInterval, outputVerticalInterval) => {
+    const controlX1 = x1 + (x2 - x1) * bezierParams.controlX1Factor;
+    const controlY1 = y1 + inputVerticalInterval * bezierParams.controlY1Factor;
+    const controlX2 = x1 + (x2 - x1) * bezierParams.controlX2Factor;
+    const controlY2 = y2 - outputVerticalInterval * bezierParams.controlY2Factor;
 
-  //   return `M${x1},${y1 + inputVerticalInterval} C${controlX1},${controlY1} ${controlX2},${controlY2} ${x2},${y2 - outputVerticalInterval}`;
-  // };
+    return `M${x1},${y1 + inputVerticalInterval} C${controlX1},${controlY1} ${controlX2},${controlY2} ${x2},${y2 - outputVerticalInterval}`;
+  };
 
   return (
     <S.Container>
-      {/* {inputTokens.map((token, i) => (
-        <S.Token
-          key={i}
-          style={{
-            left: inputWordPosCalc(i)[0],
-            top: inputWordPosCalc(i)[1],
-            width: inputWordInterval,
-          }}
-        >
-          {token}
-        </S.Token>
-      ))}
-
-      {outputTokens.map((token, i) => (
-        <S.Token
-          key={i}
-          style={{
-            left: outputWordPosCalc(i)[0],
-            top: outputWordPosCalc(i)[1],
-            width: outputWordInterval,
-          }}
-        >
-          {token}
-        </S.Token>
-      ))}
+      {tokensArr.map((tokens, layerIdx) =>
+        tokens.map((token, tokenIdx) => (
+          <S.Token
+            key={`token-${layerIdx}-${tokenIdx}`}
+            style={{
+              left: posCalcArr[layerIdx].wordPosCalc(tokenIdx)[0],
+              top: posCalcArr[layerIdx].wordPosCalc(tokenIdx)[1],
+              width: posCalcArr[layerIdx].wordInterval,
+            }}
+          >
+            {token}
+          </S.Token>
+        ))
+      )}
 
       <S.Pic>
-        {inputTokens.map((token, i) =>
-          outputTokens.map((targetToken, j) => (
-            <path
-              key={`arc-${i}-${j}`}
-              d={createBezierPath(inputWordPosCalc(i)[0], inputWordPosCalc(i)[1], outputWordPosCalc(j)[0], outputWordPosCalc(j)[1])}
-              stroke="white"
-              fill="none"
-              strokeWidth={crossSimilarityMatrix[i][j] > 0.2 ? crossSimilarityMatrix[i][j] ** 3 * 4 : 0}
-              // opacity={j == targetWordIdx || i == targetWordIdx ? 1 : 0.1}
-            />
-          ))
-        )}
-      </S.Pic> */}
+        {tokensArr
+          .slice(0, tokensArr.length - 1)
+          .map((tokens, layerIdx) =>
+            tokens.map((token, tokenIdx) =>
+              tokensArr[layerIdx + 1].map((nextToken, nextTokenIdx) => (
+                <path
+                  key={`arc-${layerIdx}-${tokenIdx}-${nextTokenIdx}`}
+                  d={createBezierPath(
+                    posCalcArr[layerIdx].wordPosCalc(tokenIdx)[0],
+                    posCalcArr[layerIdx].wordPosCalc(tokenIdx)[1],
+                    posCalcArr[layerIdx + 1].wordPosCalc(nextTokenIdx)[0],
+                    posCalcArr[layerIdx + 1].wordPosCalc(nextTokenIdx)[1],
+                    posCalcArr[layerIdx].verticalInterval,
+                    posCalcArr[layerIdx + 1].verticalInterval
+                  )}
+                  stroke="white"
+                  fill="none"
+                  strokeWidth={multiSimilarityMatrix[layerIdx][tokenIdx][nextTokenIdx] > 0.2 ? multiSimilarityMatrix[layerIdx][tokenIdx][nextTokenIdx] ** 3 * 4 : 0}
+                />
+              ))
+            )
+          )}
+      </S.Pic>
     </S.Container>
   );
 }
--- foundations/test/1-relation/3/usePosCalc.js
+++ foundations/test/1-relation/3/usePosCalc.js
@@ -1,31 +1,31 @@
-import { useMemo, useCallback, useState, useEffect } from "react";
+import { useMemo } from "react";
 import useResize from "@/utils/hooks/useResize";
 
-export default function usePosCalc({ tokens, type = null }) {
+export default function usePosCalc({ tokensArr }) {
   const [windowWidth, windowHeight] = useResize();
-  const wordLength = useMemo(() => tokens.length, [tokens]);
-  const wordInterval = useMemo(() => Math.min(0.05 * windowWidth, (windowWidth * 0.9) / wordLength), [windowWidth, wordLength]);
-  const verticalInterval = useMemo(() => windowHeight * 0.03, [windowHeight]);
 
-  const wordPosCalc = useCallback(
-    (idx) => {
-      const xPos = windowWidth / 2 - ((wordLength - 1) * wordInterval) / 2 + idx * wordInterval;
-      let yPos = windowHeight / 2;
+  const posCalcArr = useMemo(() => {
+    return tokensArr.map((tokens, idx) => {
+      const wordLength = tokens.length;
+      const wordInterval = Math.min(0.05 * windowWidth, (windowWidth * 0.9) / wordLength);
+      const verticalInterval = windowHeight * 0.02;
 
-      if (type === "input") {
-        yPos = windowHeight * 0.2;
-      } else if (type === "output") {
-        yPos = windowHeight * 0.8;
-      }
+      const yLoc = 0.1 + (0.8 * idx) / (tokensArr.length - 1);
 
-      return [xPos, yPos];
-    },
-    [wordInterval, wordLength, type]
-  );
+      const wordPosCalc = (tokenIdx) => {
+        const xPos = windowWidth / 2 - ((wordLength - 1) * wordInterval) / 2 + tokenIdx * wordInterval;
+        const yPos = windowHeight * yLoc;
 
-  return {
-    wordPosCalc,
-    wordInterval,
-    verticalInterval,
-  };
+        return [xPos, yPos];
+      };
+
+      return {
+        wordPosCalc,
+        wordInterval,
+        verticalInterval,
+      };
+    });
+  }, [tokensArr, windowWidth, windowHeight]);
+
+  return posCalcArr;
 }
