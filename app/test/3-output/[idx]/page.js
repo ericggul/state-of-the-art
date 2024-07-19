31013567b2f45a4a7e9ad2a9a904fd89e02ac9d4
ericggul <57379903+ericggul@users.noreply.github.com>
2024-07-19 19:02:54 +0900

    Feat: 1-3

--- app/test/3-output/[idx]/page.js
+++ app/test/3-output/[idx]/page.js
@@ -8,6 +8,10 @@ import { TEST_RESPONSE } from "@/foundations/test/3-output/utils/constant-en";
 //TSNE
 const Output0 = dynamic(() => import("@/foundations/test/3-output/0"));
 const Output1 = dynamic(() => import("@/foundations/test/3-output/1"));
+const Output11 = dynamic(() => import("@/foundations/test/3-output/1/1-1"));
+const Output12 = dynamic(() => import("@/foundations/test/3-output/1/1-2"));
+const Output13 = dynamic(() => import("@/foundations/test/3-output/1/1-3"));
+const Output14 = dynamic(() => import("@/foundations/test/3-output/1/1-4"));
 
 export default function TsnePage() {
   const { idx } = useParams();
@@ -16,6 +20,10 @@ export default function TsnePage() {
     <>
       {idx == "0" && <Output0 newResponse={TEST_RESPONSE} />}
       {idx == "1" && <Output1 newResponse={TEST_RESPONSE} />}
+      {idx == "1-1" && <Output11 newResponse={TEST_RESPONSE} />}
+      {idx == "1-2" && <Output12 newResponse={TEST_RESPONSE} />}
+      {idx == "1-3" && <Output13 newResponse={TEST_RESPONSE} />}
+      {idx == "1-4" && <Output14 newResponse={TEST_RESPONSE} />}
     </>
   );
 }
