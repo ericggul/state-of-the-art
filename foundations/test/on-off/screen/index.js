55035f384ed6487e6f8292dc70c7d7b8fb4f4da2
ericggul <57379903+ericggul@users.noreply.github.com>
2024-09-07 16:06:02 +0900

    Feat: On Off

--- foundations/test/on-off/screen/index.js
+++ foundations/test/on-off/screen/index.js
@@ -1,6 +1,13 @@
 import * as S from "./styles";
 
+import { useState, useEffect } from "react";
 import useSocket from "@/utils/socket/on-off/useSocketScreen";
+
+import dynamic from "next/dynamic";
+import { INPUT_EMBEDDINGS, OUTPUT_EMBEDDINGS, MULTI_LAYERS_EMBEDDINGS } from "@/foundations/test/1-relation/utils/constant-conversation";
+
+const Yakitori4 = dynamic(() => import("@/foundations/test/yakitori/0/0-4"));
+const Random26 = dynamic(() => import("@/foundations/test/1-relation/random/2/2-6"));
 
 export default function El() {
   const socket = useSocket({
@@ -8,6 +15,8 @@ export default function El() {
     handleNewMobileDisconnect,
     handleNewVisibilityChange,
   });
+
+  const [visible, setVisible] = useState(true);
 
   function handleNewMobileConnect(data) {
     console.log("new mobile connected", data);
@@ -19,7 +28,25 @@ export default function El() {
 
   function handleNewVisibilityChange(data) {
     console.log("new visibility change", data);
+    setVisible(data.isVisible);
   }
 
-  return <S.Container></S.Container>;
+  return (
+    <>
+      <S.Container
+        style={{
+          opacity: visible ? 1 : 0,
+        }}
+      >
+        <Yakitori4 enableDeviceControls={false} />
+      </S.Container>
+      <S.Container
+        style={{
+          opacity: !visible ? 1 : 0,
+        }}
+      >
+        <Random26 newInputEmbeddings={INPUT_EMBEDDINGS} newOutputEmbeddings={OUTPUT_EMBEDDINGS} />
+      </S.Container>
+    </>
+  );
 }
