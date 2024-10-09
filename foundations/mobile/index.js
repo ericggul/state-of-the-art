6af551b1e62f3257cc3074420df0282b97677276
ericggul <57379903+ericggul@users.noreply.github.com>
2024-10-09 23:25:00 +0900

    Deploy Fix

--- foundations/mobile/index.js
+++ foundations/mobile/index.js
@@ -1,15 +1,26 @@
 // Chat.js
 import React from "react";
 import * as S from "./styles";
-import { Message } from "./Message";
+import { Message } from "./message";
 import { useChatLogic } from "./utils/useChatLogic";
 import { useSuggestedResponses } from "./utils/useSuggestedResponses";
 import { useAutoScroll } from "./utils/useAutoScroll";
 
 const Chat = () => {
-  const { userInput, setUserInput, messages, inputDisabled, currentStep, showInput, placeholderText, handleSubmit, sendMessage } = useChatLogic();
+  const {
+    userInput,
+    setUserInput,
+    messages,
+    inputDisabled,
+    currentStep,
+    showInput,
+    placeholderText,
+    handleSubmit,
+    sendMessage,
+  } = useChatLogic();
 
-  const { suggestedResponses, handleSuggestedResponseClick } = useSuggestedResponses(currentStep, sendMessage);
+  const { suggestedResponses, handleSuggestedResponseClick } =
+    useSuggestedResponses(currentStep, sendMessage);
 
   const messagesEndRef = useAutoScroll(messages);
 
@@ -25,7 +36,10 @@ const Chat = () => {
       {currentStep >= 2 && suggestedResponses.length > 0 && (
         <S.SuggestedResponses>
           {suggestedResponses.map((suggestion, index) => (
-            <S.SuggestedResponseButton key={index} onClick={() => handleSuggestedResponseClick(suggestion)}>
+            <S.SuggestedResponseButton
+              key={index}
+              onClick={() => handleSuggestedResponseClick(suggestion)}
+            >
               {suggestion}
             </S.SuggestedResponseButton>
           ))}
@@ -34,7 +48,13 @@ const Chat = () => {
 
       {showInput && (
         <S.InputForm onSubmit={handleSubmit}>
-          <S.Input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} placeholder={inputDisabled ? "" : placeholderText} disabled={inputDisabled} />
+          <S.Input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder={inputDisabled ? "" : placeholderText}
+            disabled={inputDisabled}
+          />
           <S.Button type="submit" disabled={inputDisabled}>
             Send
           </S.Button>
