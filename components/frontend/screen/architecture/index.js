6e176cbc12b517271f751bcf7fcc092defa485ca
ericggul <57379903+ericggul@users.noreply.github.com>
2024-10-14 02:03:32 +0900

    Fix: ModelS Update

--- components/frontend/screen/architecture/index.js
+++ components/frontend/screen/architecture/index.js
@@ -1,7 +1,7 @@
 import * as S from "./styles";
 import Architecture3D from "@/foundations/frontend/archi/3d/v1";
 import ArchitectureUI from "@/foundations/frontend/archi/ui";
-import { MODELS } from "@/components/controller/constant/models";
+import { MODELS } from "@/components/controller/constant/models/v1";
 
 // Function to flatten the MODELS object
 const flattenModels = (models) => {
@@ -39,7 +39,16 @@ const flattenModels = (models) => {
 export default function Architecture({ version = "v4.0.2.1" }) {
   // Flatten the MODELS object
   const flattenedModels = flattenModels(MODELS);
+  const refinedFlattend = flattenedModels
+    .filter((model) => model.name !== "")
+    .map((model) => ({ name: model.name, version: model.version }));
 
+  console.log(
+    refinedFlattend,
+
+    //array with only name
+    refinedFlattend.map((model) => model.name)
+  );
   // Find relevant model from the flattened array matching with the version
   const relevantModel =
     flattenedModels.find((model) => model.version === version) ||
