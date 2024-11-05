7d8d87e13c3f121428720c9cb7c0ec9985539251
ericggul <57379903+ericggul@users.noreply.github.com>
2024-11-05 15:26:26 +0900

    Feat: Sublayer IMplementation

--- foundations/frontend/arch/Sublayer.js
+++ foundations/frontend/arch/Sublayer.js
@@ -1,81 +1,11 @@
 import React from "react";
-import { Text } from "@react-three/drei";
-import InstancedNodes from "./InstancedNodes";
-import { GRID_CONFIGS } from "../arch-models";
+import Sublayer3D from "./Sublayer-3d";
+import Sublayer2D from "./Sublayer-2d";
 
-import useScreenStore from "@/components/screen/store";
-
-export const INTERLAYER_MARGIN_X = 1.6;
-export const INTERLAYER_MARGIN_Y = 3.0;
-
-const DEFAULT_GRID = {
-  xCount: 3,
-  yCount: 3,
-  xInterval: 10,
-  yInterval: 10,
+const Sublayer = ({ is3D = false, ...props }) => {
+  return is3D ? <Sublayer3D {...props} /> : <Sublayer2D {...props} />;
 };
 
-const NON_PROJECTOR_GRID = {
-  xCount: 1,
-  yCount: 1,
-  xInterval: 10,
-  yInterval: 10,
-};
-
-const Sublayer = ({
-  position,
-  sublayer,
-  rotation,
-  style,
-  model,
-  useGivenInterval = false,
-}) => {
-  const { isProjector } = useScreenStore();
-
-  const size = sublayer.dimensions || [20, 8, 8];
-  let gridConfig = GRID_CONFIGS[model] || {};
-
-  const grid = gridConfig[sublayer.type] || DEFAULT_GRID;
-
-  return (
-    <group position={position}>
-      <InstancedNodes
-        xCount={grid.xCount}
-        yCount={grid.yCount}
-        xInterval={
-          useGivenInterval
-            ? grid.xInterval
-            : (size[0] / grid.xCount) * INTERLAYER_MARGIN_X
-        }
-        yInterval={
-          useGivenInterval
-            ? grid.yInterval
-            : (size[1] / grid.yCount) * INTERLAYER_MARGIN_Y
-        }
-        nodeSize={[size[0] / grid.xCount, size[1] / grid.yCount, size[2]]}
-        style={style}
-        color={style.colors.inner}
-        rotation={[Math.PI / 2, 0, 0]}
-        sublayer={sublayer}
-      />
-
-      {!isProjector && (
-        <group position={[0, size[1] * 0.6, 0]}>
-          <Text
-            position={[0, 0, 0]}
-            rotation={[Math.PI / 2, Math.PI, Math.PI]}
-            fontSize={(size[0] + size[1] + size[2]) * 0.05}
-            color={"white"}
-            anchorX="center"
-            anchorY="middle"
-            textAlign="center"
-          >
-            {`${sublayer.name}\n(${sublayer.type})`}
-          </Text>
-        </group>
-      )}
-    </group>
-  );
-};
+Sublayer.displayName = "Sublayer";
 
 export default Sublayer;
