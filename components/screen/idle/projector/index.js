8e4eb2a6e535f68ace362005dad77aa5c84f939e
ericggul <57379903+ericggul@users.noreply.github.com>
2024-11-18 19:10:41 +0900

    Feat: New Videos

--- components/screen/idle/projector/index.js
+++ components/screen/idle/projector/index.js
@@ -3,10 +3,24 @@
 import { memo } from "react";
 import * as S from "./styles";
 
+import useScreenStore from "@/components/screen/store";
+
+const VIDEOS = ["01_close", "01_far", "02_close", "02_far"];
+
 const Idle = memo(function Idle() {
+  const deviceIdx = useScreenStore((state) => state.deviceIndex || 0);
+  const intDeviceIdx = parseInt(deviceIdx, 10);
+
+  console.log(deviceIdx, intDeviceIdx);
+
   return (
     <S.Container>
-      <video src="/videos/test.mp4" autoPlay loop muted />
+      <video
+        src={`/videos/${VIDEOS[intDeviceIdx % VIDEOS.length]}.mp4`}
+        autoPlay
+        loop
+        muted
+      />
     </S.Container>
   );
 });
