da132680c3fc0aa76b33b221bb9bc5ed72ab9dce
ericggul <57379903+ericggul@users.noreply.github.com>
2024-12-04 20:57:29 +0900

    Detele: Deleted Unused Foundation Codes

--- components/_old/screen/v0/Intro/styles.js
+++
@@ -1,9 +0,0 @@
-import { FlexCenterStyle, WholeContainer } from "@/styles";
-import styled from "styled-components";
-
-export const Container = styled.div`
-  ${WholeContainer}
-  ${FlexCenterStyle}
-
-  flex-direction: column;
-`;
--- foundations/test-frontend/yakitori/0/0-4/layer/node.js
+++
@@ -1,8 +0,0 @@
-import React, { forwardRef } from "react";
-import { Instance } from "@react-three/drei";
-
-const Node = forwardRef(({ position, size, color, opacity = 0.4, scale }, ref) => {
-  return <Instance ref={ref} position={position} scale={size} color={color} />;
-});
-
-export default Node;
